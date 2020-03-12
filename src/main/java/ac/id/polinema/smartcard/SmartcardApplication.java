9e61a8673930989fe43c730f6ce781c4a50757ff
od3ng <noprianto@polinema.ac.id>
2020-03-12 13:49:24 +0700

    commit pertama

---
+++ src/main/java/ac/id/polinema/smartcard/SmartcardApplication.java
@@ -0,0 +1,13 @@
+package ac.id.polinema.smartcard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartcardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartcardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ac/id/polinema/smartcard/SmartcardApplicationTests.java
@@ -0,0 +1,13 @@
+package ac.id.polinema.smartcard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartcardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
